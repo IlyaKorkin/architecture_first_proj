8d525afa431596f4f45b93b429cf8e6521583a4e
IlyaKorkin <jeesusminus6@gmail.com>
2025-09-27 17:54:11 +0000

    Start of the project. sh-file was written by Vladislav Selifanov

---
+++ MainScript.sh
@@ -0,0 +1,14 @@
+echo "Введите путь до папки /log"
+read dir_path
+
+if [ -e "$dir_path" ]; then
+    echo "Путь существует: $dir_path"
+else
+    echo "Путь не существует: $dir_path"
+    until [[ -e "$dir_path" ]]; do
+        echo "Вы ввели нерпавильный путь, введите его заново"
+        read dir_path
+    done
+fi
+
+du -sh "$dir_path"
