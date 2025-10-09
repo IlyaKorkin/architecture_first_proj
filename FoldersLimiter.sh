bfce02d0be959b1eedf7c9907a006e12e439044b
IlyaKorkin <jeesusminus6@gmail.com>
2025-10-09 18:03:37 +0000

    Made files real size more predictable, started to save their real size in fstab and added instruction how to read it at the end of the script

--- FoldersLimiter.sh
+++ FoldersLimiter.sh
@@ -16,7 +16,6 @@ check_path() {
     fi
 }
 
-
 while ! check_path; do
     read dir_path
 done
@@ -30,7 +29,16 @@ until [[ "$size_mb" =~ ^[0-9]+$ ]] && [[ "$size_mb" -ge 16 ]] && [[ "$size_mb" -
     echo "Size must be an integer between 16 and 1000 MB."
     read size_mb
 done
-size_mb=$(((size_mb * 105) / 100 + 6))
+
+
+if [ "$size_mb" -lt 500 ]; then
+    journal_mb=4
+else
+    journal_mb=8
+fi
+
+img_size_mb=$(( ((size_mb + journal_mb  + 1) * 64 + 62) / 63 ))
+
 
 echo "Enter the path to store the .img files:"
 read img_storage_path
@@ -47,19 +55,23 @@ fi
 
 img_file="$img_storage_path/limiter_$(basename "$dir_path").img"
 
-echo "Creating image file: $img_file ($size_mb MB)"
-dd if=/dev/zero of="$img_file" bs=1M count="$size_mb" || { echo "Failed to create image file"; exit 1; }
+echo "Creating image file: $img_file ($img_size_mb MB)"
+dd if=/dev/zero of="$img_file" bs=1MB count="$img_size_mb" || { echo "Failed to create image file"; exit 1; }
 
-sudo mkfs.ext4 "$img_file" -F || { echo "Failed to format image"; exit 1; }
-
+sudo mkfs.ext4 "$img_file" -F -m 0 -i 16384 -J size=$journal_mb || { echo "Failed to format image"; exit 1; }
 sudo mount -o loop "$img_file" "$dir_path" || { echo "Failed to mount image"; exit 1; }
 
 current_user=$(whoami)
 sudo chown -R "$current_user:$current_user" "$dir_path" || { echo "Failed to set permissions"; exit 1; }
 
-fstab_entry="$img_file $dir_path ext4 loop,defaults,uid=$(id -u),gid=$(id -g) 0 2"
+avail_size=$(df -H --output=avail "$dir_path" | tail -1 | sed 's/ //g')
+fstab_entry="$img_file $dir_path ext4 loop,defaults,uid=$(id -u),gid=$(id -g) 0 2 # avail=$avail_size"
 echo "$fstab_entry" | sudo tee -a /etc/fstab || { echo "Failed to update /etc/fstab"; exit 1; }
 
 
-df -h "$dir_path" > /dev/null 2>&1 || { echo "Critical fail"; exit 1; }
-echo "Folder $dir_path is now limited to $size_mb MB."
+df -H "$dir_path"
+echo "Folder $dir_path is now limited to $avail_size MB."
+
+#How to read a folder's size
+#dir_path=Your path
+#folder_size=$(grep "$dir_path" /etc/fstab | grep -o 'avail=[0-9]*' | sed 's/avail=//')
